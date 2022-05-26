1586e9b0752a4c07ffd924a45289245e30aaf716
Pablerdo <pablosalamanca88@gmail.com>
2022-05-25 19:44:43 -0700

    Profile page review

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -13,10 +13,7 @@ const account = {
   totalCoins: faker.datatype.number({ min: 100, max: 10000, precision: 1 }),
   outstandingCoins: faker.datatype.number({ min: 50, max: 1000, precision: 1 }),
   equityPaidOut: faker.datatype.number({ min: 10000, max: 100000, precision: 1 }),
-
-
-
-
+  leaderboardStanding: faker.datatype.number({ min: 1, max: 100, precision: 1 }),
 };
 
 export default account;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,32 @@
-import { Link as RouterLink } from 'react-router-dom';
 // material
-import { Grid, Button, Container, Stack, Typography } from '@mui/material';
+import {Grid, Container, Typography, Card, CardContent, Avatar} from '@mui/material';
 // components
+import {styled} from "@mui/material/styles";
 import Page from '../components/Page';
-import Iconify from '../components/Iconify';
-import { ProfileCard } from '../sections/@dashboard/profile';
-// mock
-import POSTS from '../_mock/blog';
+
+import ACCOUNT from "../_mock/account";
 
 // ----------------------------------------------------------------------
 
-const SORT_OPTIONS = [
-  { value: 'latest', label: 'Latest' },
-  { value: 'popular', label: 'Popular' },
-  { value: 'oldest', label: 'Oldest' },
-];
+const CardMediaStyle = styled('div')({
+  position: 'relative',
+  paddingTop: 'calc(15%)',
+});
+
+
+const AvatarStyle = styled(Avatar)(({ theme }) => ({
+  zIndex: 9,
+  width: 220,
+  height: 220,
+  position: 'absolute',
+  left: theme.spacing(5),
+  bottom: theme.spacing(0),
+
+
+}));
+
+
+
 
 // ----------------------------------------------------------------------
 
@@ -23,11 +35,53 @@ export default function Profile() {
     <Page title="Dashboard: Profile">
       <Container>
 
-        <Grid container spacing={3}>
-          {POSTS.map((post, index) => (
-            <ProfileCard key={post.id} post={post} index={index} />
-          ))}
-        </Grid>
+
+            <Card sx={{ position: 'relative' }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={12} md={12}>
+                <CardMediaStyle>
+
+                  <AvatarStyle
+                    alt={ACCOUNT.displayName}
+                    src='https://directory.seas.upenn.edu/wp-content/uploads/2020/03/Greenberg-Clayton.jpg'
+                    style={{
+                      border: '4px solid lightgray'
+                    }}
+                />
+
+                  {/* <CoverImgStyle alt="Account's Header" src='../images/angryimg.png' /> */}
+                  <Typography variant='h2' sx={{marginLeft: '350px'}}>
+                    {ACCOUNT.displayName}
+                  </Typography>
+              </CardMediaStyle>
+
+              <CardContent
+                  sx={{
+                    pt: 4,
+                  }}
+              >
+                <Typography variant='h5' gutterBottom>
+                  Age: <span style={{fontWeight: 300}}>{ACCOUNT.age}</span>
+                </Typography>
+                <Typography variant='h5' gutterBottom>
+                  Occupation: <span style={{fontWeight: 300}}>{ACCOUNT.occupation}</span>
+                </Typography>
+                <Typography variant='h5' gutterBottom>
+                  Location: <span style={{fontWeight: 300}}>{ACCOUNT.location}</span>
+                </Typography>
+                <Typography variant='h5' gutterBottom>
+                  Email: <span style={{fontWeight: 300}}>{ACCOUNT.email}</span>
+                </Typography>
+                <Typography variant='h5'>
+                  Standing: <span style={{fontWeight: 300}}>#{ACCOUNT.leaderboardStanding}</span>
+                </Typography>
+
+
+              </CardContent>
+                </Grid>
+              </Grid>
+            </Card>
+
       </Container>
     </Page>
   );
