699804374380581287e55d524a05221948d15d55
Kaushik Akula <akula.kaushik.akula@gmail.com>
2022-05-31 00:37:12 -0400

    add firebase in

--- backend/endpoints/main.py
+++ backend/endpoints/main.py
@@ -1,11 +1,36 @@
 from flask import Flask
+from flask_restx import Api, Resource
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-app = Flask(__name__)
+cred = credentials.Certificate("valued-throne-350421-firebase-adminsdk-8of5y-cc6d986bb9.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+flaskApp = Flask(__name__)
+
 baseRoute = "/api"
 businessRoute = "/businesses"
-tryvestorRoute = "/tryvestor"
+tryvestorRoute = "/tryvestors"
+api = Api(app=flaskApp, prefix=baseRoute)
+busApi = api.namespace('businesses', description="For business side requests")
+tryApi = api.namespace('tryvestors', description="For tryvestor side requests")
+
+@busApi.route("/")
+class busHello(Resource):
+    def get(self):
+        return 'Business Hello, Get!'
+
+    def post(self):
+        return 'Business Hello, Post!'
+
+@tryApi.route("/")
+class tryHello(Resource):
+    def get(self):
+        return 'Tryvestor Hello, Get!'
+
+    def post(self):
+        return 'Tryvestor Hello, Post!'
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+flaskApp.run()
