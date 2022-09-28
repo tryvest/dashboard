63f3edf2f1dc7e5f649572d0f4782668e66d5160
Kaushik Akula <akula.kaushik.akula@gmail.com>
2022-09-27 20:02:38 -0400

    adding transactions table

---
+++ src/components/TransactionsTable.js
@@ -0,0 +1,332 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import { fCurrency, fPercent } from '../utils/formatNumber';
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'institutionLogo',
+    numeric: false,
+    disablePadding: false,
+    label: 'ACCOUNT',
+  },
+  {
+    id: 'businessLogo',
+    numeric: false,
+    disablePadding: false,
+    label: 'COMPANY',
+  },
+  {
+    id: 'transactionDate',
+    numeric: true,
+    disablePadding: false,
+    label: 'DATE',
+  },
+  {
+    id: 'transactionAmount',
+    numeric: true,
+    disablePadding: false,
+    label: 'AMOUNT',
+  },
+  {
+    id: 'percentStockback',
+    numeric: true,
+    disablePadding: false,
+    label: '% STOCK-BACK',
+  },
+  {
+    id: 'amountMoneyInvested',
+    numeric: true,
+    disablePadding: false,
+    label: '$ STOCK INVESTED',
+  },
+];
+
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={'center'}
+            // align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { xs: 1, sm: 2 },
+        pr: { xs: 1, sm: 1 },
+      }}
+    >
+      <Typography fontWeight={800} fontSize={20}>
+        Your Recent Transactions
+      </Typography>
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
+export default function EnhancedTable(props) {
+  let { transactions } = props;
+  const numTransactions = 5;
+  transactions = transactions.slice(0, numTransactions);
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('date');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(numTransactions);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  // const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2, pb: 2}}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        {transactions.size > 0 ? (
+          <div>
+            <TableContainer>
+              <Table sx={{ minWidth: 600 }} aria-labelledby="tableTitle" size={dense ? 'small' : 'medium'}>
+                <EnhancedTableHead
+                  numSelected={selected.length}
+                  order={order}
+                  orderBy={orderBy}
+                  onSelectAllClick={() => {
+                    return 'hi';
+                  }}
+                  onRequestSort={handleRequestSort}
+                  rowCount={transactions.length}
+                />
+                <TableBody>
+                  {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+                  {stableSort(transactions, getComparator(order, orderBy))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((transaction, index) => {
+                      const labelId = `enhanced-table-checkbox-${index}`;
+
+                      return (
+                        <TableRow hover tabIndex={-1} key={index}>
+                          <TableCell component="th" id={labelId} scope="row" padding="none" align={'center'}>
+                            <img style={{ maxHeight: '40px' }} src={transaction.institutionLogo} alt="bank logo" />
+                          </TableCell>
+                          <TableCell align="center">
+                            {
+                              <img
+                                style={{ borderRadius: '10px' }}
+                                height={'40px'}
+                                width={'100px'}
+                                src={transaction.businessLogo}
+                                alt="business logo"
+                              />
+                            }
+                          </TableCell>
+                          <TableCell align="center">
+                            {new Date(transaction.transactionDate).toLocaleDateString()}
+                          </TableCell>
+                          <TableCell align="center">{fCurrency(transaction.transactionAmount)}</TableCell>
+                          <TableCell align="center">{fPercent(transaction.percentStockback * 100)}</TableCell>
+                          <TableCell align="center">
+                            {<div style={{ color: 'green' }}>{fCurrency(transaction.amountMoneyInvested)}</div>}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <div style={{ width: '100%', display: 'flex', alignItems: 'end' }}>
+              <Typography marginLeft={'auto'} marginRight={'15px'} fontSize={14} color={'#0000EE'}>
+                See All
+              </Typography>
+            </div>
+          </div>
+        ) : (
+          <Typography fontStyle={'italic'} marginLeft={'15px'} marginRight={'5px'} fontSize={15}>
+            Transactions will appear here once you connect an account!
+          </Typography>
+        )}
+      </Paper>
+    </Box>
+  );
+}
+
+// Beautiful Table: https://mui.com/material-ui/react-table/
+/*
+function createData(name, calories, fat, carbs, protein) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+  };
+}
+
+const rows = [
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Donut', 452, 25.0, 51, 4.9),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Honeycomb', 408, 3.2, 87, 6.5),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Jelly Bean', 375, 0.0, 94, 0.0),
+  createData('KitKat', 518, 26.0, 65, 7.0),
+  createData('Lollipop', 392, 0.2, 98, 0.0),
+  createData('Marshmallow', 318, 0, 81, 2.0),
+  createData('Nougat', 360, 19.0, 9, 37.0),
+  createData('Oreo', 437, 18.0, 63, 4.0),
+];
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.name);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+// Avoid a layout jump when reaching the last page with empty rows.
+              const emptyRows =
+              page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+
+ */
