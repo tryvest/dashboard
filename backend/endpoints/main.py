0776b2f8e66278457a18864a5bb34322d0d84f84
Kaushik Akula <akula.kaushik.akula@gmail.com>
2022-10-28 21:00:53 -0400

    need to update release with correct api

--- backend/endpoints/main.py
+++ backend/endpoints/main.py
@@ -681,20 +681,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Fill in your Plaid API keys - https://dashboard.plaid.com/account/keys
-PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID', "62ef0e6f20b6d70013c6dd0a")
-PLAID_SECRET = os.getenv('PLAID_SECRET', "67b72d84fca21565d31219bf20b357")
+PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
+PLAID_SECRET = os.getenv('PLAID_SECRET')
 # Use 'sandbox' to test with Plaid's Sandbox environment (username: user_good,
 # password: pass_good)
 # Use `development` to test with live users and credentials and `production`
 # to go live
-PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+PLAID_ENV = os.getenv('PLAID_ENV')
 # PLAID_PRODUCTS is a comma-separated list of products to use when initializing
 # Link. Note that this list must contain 'assets' in order for the app to be
 # able to create and retrieve asset reports.
-PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS', 'transactions, auth').split(',')
+PLAID_PRODUCTS = os.getenv('PLAID_PRODUCTS').split(',')
 # PLAID_COUNTRY_CODES is a comma-separated list of countries for which users
 # will be able to select institutions from.
-PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES', 'US, CA').split(',')
+PLAID_COUNTRY_CODES = os.getenv('PLAID_COUNTRY_CODES').split(',')
 PLAID_REDIRECT_URI = None
 
 host = plaid.Environment.Sandbox
